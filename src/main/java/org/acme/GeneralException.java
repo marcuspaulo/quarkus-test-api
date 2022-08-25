51c3c587060041d4de45e8b64cf03a80cbafe8b5
Marcus Paulo <mpaulobr@gmail.com>
2022-08-25 23:48:45 +0100

    Input received date dd/MM/yyyy

--- src/main/java/org/acme/GeneralException.java
+++ src/main/java/org/acme/GeneralException.java
@@ -1,6 +1,5 @@
 package org.acme;
 
-import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
--- src/main/java/org/acme/Input.java
+++ src/main/java/org/acme/Input.java
@@ -1,9 +1,13 @@
 package org.acme;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 
 public class Input {
     private String name;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate birthday;
 
     public String getName() {
